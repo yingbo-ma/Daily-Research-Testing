3a9f9876fa1a92849dc657acdc2b741fe917b86c
yingbo-ma <yb.ma95@gmail.com>
2022-05-02 22:09:22 -0400

    update gpu_testing.py

--- GANs/gpu_testing.py
+++ GANs/gpu_testing.py
@@ -4,3 +4,7 @@ print(device_lib.list_local_devices())
 
 import tensorflow as tf
 tf.test.is_gpu_available()
+
+physical_device = tf.config.experimental.list_physical_devices('GPU')
+print(f'Device found : {physical_device}')
+tf.config.experimental.set_memory_growth(physical_device[0],True)
